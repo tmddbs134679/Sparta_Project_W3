05efa66c5693e89c35635f75b3c42af406871916
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2025-04-30 19:01:07 +0900

    feat : 고블린, 오크 몬스터 Animation 추가 및 EnemyManager 등 몬스터 로직 관리 구현

---
+++ Assets/PJ2/02.Scirpts/Entity/EnemyController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : BaseController
+{
+    private EnemyManager enemyManager;
+    private Transform target;
+
+    [SerializeField] private float followRange = 15f;
+
+    public void Init(EnemyManager enemyManager, Transform target)
+    {
+        this.enemyManager = enemyManager;
+        this.target = target;
+    }
+
+
+    protected float DistanceToTarget()
+    {
+        return Vector3.Distance(transform.position, target.position);
+    }
+
+    protected Vector2 DirectionToTarget()
+    {
+        return (target.position - transform.position).normalized;
+    }
+
+    protected override void HandleAction()
+    {
+        base.HandleAction();
+        
+        if(weaponHandler == null || target == null ) 
+        {
+            if(!movementDir.Equals(Vector2.zero))
+            {
+                movementDir = Vector2.zero;
+                return;
+            }
+        }
+
+        float dis = DistanceToTarget();
+        Vector2 dir = DirectionToTarget();
+
+        isAttacking = false;
+
+        if(dis <= followRange)
+        {
+            lookDir = dir;
+
+            if(dis < weaponHandler.attackRange)
+            {
+                int layerMaskTarget = weaponHandler.target;
+                RaycastHit2D hit = Physics2D.Raycast(transform.position,
+                                                     dir,
+                                                     weaponHandler.attackRange * 1.5f,
+                                                     (1 << LayerMask.NameToLayer("Level")) | layerMaskTarget);
+
+                if(hit.collider != null && layerMaskTarget == (layerMaskTarget | (1 << hit.collider.gameObject.layer)))
+                {
+                    isAttacking = true;
+                }
+
+                movementDir = Vector2.zero;
+                return;
+            }
+
+            movementDir = dir;
+        }
+    }
+
+}
---
+++ Assets/PJ2/02.Scirpts/Manager/EnemyManager.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemyManager : MonoBehaviour
+{
+    private Coroutine waveRoutine;
+
+    [SerializeField] private List<GameObject> enemyPrefabs;
+
+    [SerializeField] List<Rect> spawnAreas;
+    [SerializeField] private Color gizmoColror = new Color(1, 0, 0, .3f);
+    private List<EnemyController> activeEnemies = new List<EnemyController>();
+
+    private bool enemySpawnCompliete;
+
+    [SerializeField] private float timeBetweenSpawns = 0.2f;
+    [SerializeField] private float timeBetweenWaves = 1f;
+
+
+    public void StartWave(int waveCount)
+    {
+        if(waveRoutine != null)
+            StopCoroutine(waveRoutine);
+        waveRoutine = StartCoroutine(SpawnWave(waveCount));
+    }
+
+
+    public void StopWave()
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator SpawnWave(int waveCount)
+    {
+        enemySpawnCompliete = false;
+        yield return new WaitForSeconds(timeBetweenSpawns);
+
+        for(int i = 0; i < waveCount; i++)
+        {
+            yield return new WaitForSeconds(timeBetweenSpawns);
+            SpawnRandomEnemy();
+        }
+
+        enemySpawnCompliete = true;
+
+    }
+
+    private void SpawnRandomEnemy()
+    {
+       if(enemyPrefabs.Count == 0 || spawnAreas.Count == 0)
+        {
+            Debug.LogWarning("¼³Á¤ x");
+            return;
+        }
+
+        GameObject randomPrefab = enemyPrefabs[Random.Range(0, enemyPrefabs.Count)];
+
+
+        Rect randomArea = spawnAreas[Random.Range(0, spawnAreas.Count)];
+
+        Vector2 randomPos = new Vector2
+            (
+                Random.Range(randomArea.xMin, randomArea.xMax),
+                Random.Range(randomArea.yMin, randomArea.yMax));
+
+        GameObject spawnEnemy = Instantiate(randomPrefab, new Vector3(randomPos.x, randomPos.y), Quaternion.identity);
+        EnemyController enemyController = spawnEnemy.GetComponent<EnemyController>();
+
+        activeEnemies.Add(enemyController);
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (spawnAreas == null) return;
+         
+        Gizmos.color = gizmoColror;
+        foreach(var area in spawnAreas)
+        {
+            Vector3 center = new Vector3(area.x + area.width / 2, area.y + area.height / 2);
+            Vector3 size = new Vector3(area.width, area.height);
+
+            Gizmos.DrawCube(center, size);
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartWave(1);
+        }    
+    }
+}
